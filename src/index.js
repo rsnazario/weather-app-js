f7fce748c8cae5749eb06e0cc9e4e59a1f49e0af
rsnazario <rafael.s.nazario@gmail.com>
2020-09-09 18:38:36 -0300

    correctly retrieving data from api source

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 console.log('Application has Started');
 
 async function gets() {
-  const result = await fetch('api.openweathermap.org/data/2.5/weather?q=birigui&appid=7dd12ed8bc61861e26671e1bee6f88de', {mode: 'cors'});
+  console.log('bla');
+
+  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Birigui&appid=7dd12ed8bc61861e26671e1bee6f88de', {mode: 'cors'});
+  const result = await response.json();
+  document.getElementById('content').innerHTML = result.main.temp;
+  console.log(response);
   console.log(result);
 }
+
+gets();
