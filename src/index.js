f44f48762ba23457a34a14fff782094042df7989
rsnazario <rafael.s.nazario@gmail.com>
2020-09-10 13:02:58 -0300

    webpack watch script installed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 console.log('Application has Started');
 
+const key = '7dd12ed8bc61861e26671e1bee6f88de';
 async function gets() {
   console.log('bla');
 
-  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Birigui&appid=7dd12ed8bc61861e26671e1bee6f88de', {mode: 'cors'});
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Birigui&units=metric&appid=${key}`);
   const result = await response.json();
-  document.getElementById('content').innerHTML = result.main.temp;
+  
   console.log(response);
   console.log(result);
 }
