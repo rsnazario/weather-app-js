cadcdf04803822a0392c3d53467393709521e007
rsnazario <rafael.s.nazario@gmail.com>
2020-09-10 22:26:22 -0300

    connected search engine to event listener

--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
-console.log('Application has Started');
-
 const key = '7dd12ed8bc61861e26671e1bee6f88de';
-async function gets() {
-  console.log('bla');
 
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Birigui&units=metric&appid=${key}`);
-  const result = await response.json();
-  
-  console.log(response);
-  console.log(result);
-}
+var searchEngine = (function() {
+  async function searchWeather(city) {
 
-gets();
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`);
+    const result = await response.json();
+    
+    console.log(result);
+  };
+
+  return { searchWeather };
+})();
+// gets();
+// ---------------------------------------------
+
+
+var mainController = (function(searcher) {
+
+  var triggerSearch = function() {
+    const bla = document.querySelector('input');
+    if (bla.value !== '') {
+      let val = bla.value;
+      bla.value = '';
+      searcher.searchWeather(val);
+
+    }
+    else {
+      console.log('empty');
+    }
+  }
+
+  var setupEventListeners = function() {
+    document.querySelector('button').addEventListener('click', triggerSearch);
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13) {
+        triggerSearch();
+      }
+    });
+  };
+
+  return {
+    init: function() {
+      console.log('Application has Started');
+      setupEventListeners();
+    }
+  }
+})(searchEngine);
+
+mainController.init();
